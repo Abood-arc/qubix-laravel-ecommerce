ebed062466f6faaa3afe55c34621c4f263a64c12
Abood-arc <abdullahafaq17@gmail.com>
2026-08-30 01:58:02 +0500

    feat(rtl): wire arabic locale into the storefront date picker

--- packages/DigitalLabs/Shop/src/Resources/assets/js/plugins/flatpickr.js
+++ packages/DigitalLabs/Shop/src/Resources/assets/js/plugins/flatpickr.js
@@ -1,8 +1,25 @@
 import Flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.css";
+import { Arabic } from "flatpickr/dist/l10n/ar.js";
 
 export default {
     install: (app) => {
         window.Flatpickr = Flatpickr;
+
+        const setLocaleFromLang = () => {
+            const lang = document.documentElement.lang || "en";
+
+            const localeMap = {
+                ar: Arabic,
+            };
+
+            const locale = localeMap[lang] || null;
+
+            if (locale) {
+                window.Flatpickr.localize(locale);
+            }
+        };
+
+        setLocaleFromLang();
     },
 };
